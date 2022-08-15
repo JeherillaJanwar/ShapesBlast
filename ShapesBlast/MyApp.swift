5446cf2d765e0275fff447e88ff055872be3d6ad
Ishaan Sharma <ishaan328069@gmail.com>
2022-08-15 10:48:03 -0400

    Create MyApp.swift

---
+++ ShapesBlast/MyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyApp.swift
+//  ShapesBlast
+//
+//  Created by Ishaan Sharma on 7/29/2022.
+//
+
+import SwiftUI
+
+@main
+struct ShapesBlast: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
